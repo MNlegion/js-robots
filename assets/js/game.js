3bb2781d84635f3469cb927b84f6bb5d57d3459c
MNlegion <rkreuser30@gmail.com>
2023-08-17 21:09:03 -0400

    initial fight stage interaction

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,57 @@
 var playerName = window.prompt("What is your fighter's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerHealth, playerAttack);
 
-function fight() {
-    window.alert("Start Fight!");
-}
+var enemyName = "Optimus";
+var enemyHealth = 40;
+var enemyAttack = 8;
 
-// fight();
+var fight = function () {
+  window.alert("Welcome to the Arena!");
+
+  // Player Attack Enemy
+  enemyHealth = enemyHealth - playerAttack;
+
+  console.log(
+    playerName +
+      " attacked " +
+      enemyName +
+      "! " +
+      enemyName +
+      " now has " +
+      enemyHealth +
+      " health remaining."
+  );
+
+  // Check Enemy Health is > 0
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " has " + enemyHealth + " health remaining!");
+  }
+
+  // Enemy Attack Player
+  playerHealth = playerHealth - enemyAttack;
+
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+};
+
+fight();
